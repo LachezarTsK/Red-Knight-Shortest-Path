0fa52314b468ce6840cb9e5d53cc32f3f804c98b
Lachezar Kutsarov <lkutsarov@gmail.com>
2019-06-13 15:55:23 +0200

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -8,42 +8,55 @@ import java.util.Scanner;
 public class Solution {
 
 	private static int chessboardSide;
+	private static int startRow;
+	private static int startColumn;
+	private static int goalRow;
+	private static int goalColumn;
+	private static Position start;
+	private static Position goal;
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		chessboardSide = scanner.nextInt();
-		int startRow = scanner.nextInt();
-		int startColumn = scanner.nextInt();
-		int goalRow = scanner.nextInt();
-		int goalColumn = scanner.nextInt();
+		startRow = scanner.nextInt();
+		startColumn = scanner.nextInt();
+		goalRow = scanner.nextInt();
+		goalColumn = scanner.nextInt();
 		scanner.close();
 
-		getResults(startRow, startColumn, goalRow, goalColumn);
+		printResults();
 	}
 
 	/**
-	 * If the goal can be reached, the method traces back the path with minimum
-	 * number of moves. This path is stored in List "pathWithMinimumMoves".
+	 * If the goal can be reached, the method prints the minimum number of moves on
+	 * one line. On another line, it prints the type of every move (UL, UR, R, LR,
+	 * LL, L) to reach each position on the path.
 	 * 
-	 * This list contains all positions on the path, except the start. 
-         * Thus, the size of the list is printed for the value of minimum number of moves.
-	 * 
-	 * Then, on the next line, the type of move to reach each position on the path
-	 * is printed.
-	 * 
-	 * If the goal is unreachable, i.e. "searchPath_minimumNumberOfMoves(...)"
-	 * returns null, the method prints "Impossible".
+	 * If the goal is unreachable, the method prints "Impossible".
 	 */
-	private static void getResults(int startRow, int startColumn, int endRow, int endColumn) {
-
+	private static void printResults() {
 		Map<Position, Position> tracePathMap = new HashMap<Position, Position>();
-		Position goal = searchPath_minimumNumberOfMoves(startRow, startColumn, endRow, endColumn, tracePathMap);
+		boolean goalHasBeenReached = searchPath_minimumNumberOfMoves(tracePathMap);
 
-		if (goal == null) {
+		if (goalHasBeenReached == false) {
 			System.out.println("Impossible");
 			return;
 		}
+		List<Position> pathWithMinimumMoves = traceBack_PathWithMinimumMoves(tracePathMap);
+		int numberOfMoves = pathWithMinimumMoves.size() - 1;
 
+		System.out.println(numberOfMoves);
+		for (int i = pathWithMinimumMoves.size() - 2; i >= 0; i--) {
+			System.out.print(pathWithMinimumMoves.get(i).typeOfMove_fromPreviousToThisPosition + " ");
+		}
+	}
+
+	/**
+	 * The method traces back the path with minimum number ofo moves.
+	 * 
+	 * @return List that contains the positions on this path, from start to goal.
+	 */
+	private static List<Position> traceBack_PathWithMinimumMoves(Map<Position, Position> tracePathMap) {
 		List<Position> pathWithMinimumMoves = new ArrayList<Position>();
 		Position current = tracePathMap.get(goal);
 		pathWithMinimumMoves.add(goal);
@@ -52,34 +65,28 @@ public class Solution {
 			pathWithMinimumMoves.add(current);
 			current = tracePathMap.get(current);
 		}
+		pathWithMinimumMoves.add(start);
 
-		int moves_shortestPath = pathWithMinimumMoves.size();
-		System.out.println(moves_shortestPath);
-
-		for (int i = pathWithMinimumMoves.size() - 1; i >= 0; i--) {
-			System.out.print(pathWithMinimumMoves.get(i).moveToReachPosition + " ");
-		}
+		return pathWithMinimumMoves;
 	}
 
 	/**
-	 * The method applies breadth first search in order to find the path with least
-	 * number of moves from start to goal.
+	 * The method applies breadth first search to seek the path with least number of
+	 * moves from start to goal.
 	 * 
-	 * There might be multiple paths for the minimum number of moves. In this case,
-	 * the path that is printed as a result, is the path that prioritizes the moves
-	 * in the following order: UpperLeft, UpperRight, Right, LowerRight, LowerLeft,
-	 * Left.
+	 * There might be multiple paths for the minimum number of moves. The result
+	 * that is printed, is the path that prioritizes the moves in the following
+	 * order: UpperLeft, UpperRight, Right, LowerRight, LowerLeft, Left.
 	 * 
 	 * The method also fills HashMap "tracePathMap", which is applied to trace 
-         * the path with minimum number of moves.
+	 * the path with minimum number of moves.
 	 * 
-	 * @return Object of class "Position" that represents the goal. 
-         *         If the goal is not reachable, then "null" is returned.
+	 * @return true, if the goal can be reached. Otherwise, false.
 	 */
-	private static Position searchPath_minimumNumberOfMoves(int startRow, int startColumn, int endRow, int endColumn,
-			Map<Position, Position> tracePathMap) {
+	private static boolean searchPath_minimumNumberOfMoves(Map<Position, Position> tracePathMap) {
 		LinkedList<Position> chessboardPositions = new LinkedList<Position>();
-		Position current = new Position(startRow, startColumn);
+		start = new Position(startRow, startColumn);
+		Position current = start;
 		chessboardPositions.add(current);
 
 		boolean[][] visited = new boolean[chessboardSide][chessboardSide];
@@ -90,8 +97,9 @@ public class Solution {
 		while (!chessboardPositions.isEmpty()) {
 			current = chessboardPositions.removeFirst();
 
-			if (current.row == endRow && current.column == endColumn) {
-				return current;
+			if (current.row == goalRow && current.column == goalColumn) {
+				goal = current;
+				return true;
 			}
 
 			/**
@@ -101,7 +109,7 @@ public class Solution {
 			column = current.column - 1;
 			if (isOnChessboard(row, column) && !visited[row][column]) {
 				Position next = new Position(row, column);
-				next.moveToReachPosition = "UL";
+				next.typeOfMove_fromPreviousToThisPosition = "UL";
 				chessboardPositions.add(next);
 				visited[row][column] = true;
 				tracePathMap.put(next, current);
@@ -114,7 +122,7 @@ public class Solution {
 			column = current.column + 1;
 			if (isOnChessboard(row, column) && !visited[row][column]) {
 				Position next = new Position(row, column);
-				next.moveToReachPosition = "UR";
+				next.typeOfMove_fromPreviousToThisPosition = "UR";
 				chessboardPositions.add(next);
 				visited[row][column] = true;
 				tracePathMap.put(next, current);
@@ -127,7 +135,7 @@ public class Solution {
 			column = current.column + 2;
 			if (isOnChessboard(row, column) && !visited[row][column]) {
 				Position next = new Position(row, column);
-				next.moveToReachPosition = "R";
+				next.typeOfMove_fromPreviousToThisPosition = "R";
 				chessboardPositions.add(next);
 				visited[row][column] = true;
 				tracePathMap.put(next, current);
@@ -140,7 +148,7 @@ public class Solution {
 			column = current.column + 1;
 			if (isOnChessboard(row, column) && !visited[row][column]) {
 				Position next = new Position(row, column);
-				next.moveToReachPosition = "LR";
+				next.typeOfMove_fromPreviousToThisPosition = "LR";
 				chessboardPositions.add(next);
 				visited[row][column] = true;
 				tracePathMap.put(next, current);
@@ -153,7 +161,7 @@ public class Solution {
 			column = current.column - 1;
 			if (isOnChessboard(row, column) && !visited[row][column]) {
 				Position next = new Position(row, column);
-				next.moveToReachPosition = "LL";
+				next.typeOfMove_fromPreviousToThisPosition = "LL";
 				chessboardPositions.add(next);
 				visited[row][column] = true;
 				tracePathMap.put(next, current);
@@ -166,21 +174,20 @@ public class Solution {
 			column = current.column - 2;
 			if (isOnChessboard(row, column) && !visited[row][column]) {
 				Position next = new Position(row, column);
-				next.moveToReachPosition = "L";
+				next.typeOfMove_fromPreviousToThisPosition = "L";
 				chessboardPositions.add(next);
 				visited[row][column] = true;
 				tracePathMap.put(next, current);
 			}
 		}
-
-		return null;
+		return false;
 	}
 
 	/**
 	 * The method checks whether the neighboring row and column are within 
-         * the boundaries of the chess board.
+	 * the boundaries of the chess board.
 	 * 
-	 * @return true, if they are on the chess board. Otherwise, it returns false.
+	 * @return true, if they are on the chess board. Otherwise, false.
 	 */
 	private static boolean isOnChessboard(int row, int column) {
 		if (row < 0 || row >= chessboardSide || column < 0 || column >= chessboardSide) {
@@ -192,12 +199,13 @@ public class Solution {
 
 /**
  * Class "Position" stores the coordinates of a point on the chess board and 
- * the type of the last move to reach this point (UL, UR, R, LR, LL, L).
+ * the type of the move (UL, UR, R, LR, LL, L) from the previous position to this
+ * position.
  */
 class Position {
 	int row;
 	int column;
-	String moveToReachPosition;
+	String typeOfMove_fromPreviousToThisPosition;
 
 	public Position(int row, int column) {
 		this.row = row;
